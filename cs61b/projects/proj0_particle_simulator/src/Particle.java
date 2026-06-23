c76fa8f1b15a823c2f0b3853aac04b1f5c08aae9
AdityaMane-cell <adityamane.sae.comp@gmail.com>
2026-06-23 14:26:50 +0530

    add: adding skeleton structure for project 0: particle simulator

---
+++ cs61b/projects/proj0_particle_simulator/src/Particle.java
@@ -0,0 +1,5 @@
+package cs61b.projects.proj0_particle_simulator.src;
+
+public class Particle {
+
+}
---
+++ cs61b/projects/proj0_particle_simulator/tests/TestParticle.java
@@ -0,0 +1,5 @@
+package cs61b.projects.proj0_particle_simulator.tests;
+
+public class TestParticle {
+
+}
