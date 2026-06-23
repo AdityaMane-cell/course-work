17503dd4e9b96d6710ab0e70a2319de531164340
AdityaMane-cell <adityamane.sae.comp@gmail.com>
2026-06-23 14:42:42 +0530

    udpate: updating proj0 skeleton: missing edu packages

--- cs61b/projects/proj0_particle_simulator/tests/TestParticle.java
+++ cs61b/projects/proj0_particle_simulator/tests/TestParticle.java
@@ -1,5 +1,46 @@
 package cs61b.projects.proj0_particle_simulator.tests;
 
-public class TestParticle {
+import java.awt.*;
+import java.util.Map;
 
+public class TestParticle {
+    public ParticleFlavor flavor;
+    public int lifespan;
+
+    public static final int PLANT_LIFESPAN = 150;
+    public static final int FLOWER_LIFESPAN = 75;
+    public static final int FIRE_LIFESPAN = 10;
+    public static final Map<ParticleFlavor, Integer> LIFESPANS = Map.of(ParticleFlavor.FLOWER, FLOWER_LIFESPAN,
+            ParticleFlavor.PLANT, PLANT_LIFESPAN,
+            ParticleFlavor.FIRE, FIRE_LIFESPAN);
+
+    public Particle(ParticleFlavor flavor) {
+        this.flavor = flavor;
+        lifespan = -1;
+    }
+
+    public Color color() {
+        if (flavor == ParticleFlavor.EMPTY) {
+            return Color.BLACK;
+        }
+        return Color.GRAY;
+    }
+
+    public void moveInto(Particle other) {
+    }
+
+    public void fall(Map<Direction, Particle> neighbors) {
+    }
+
+    public void flow(Map<Direction, Particle> neighbors) {
+    }
+
+    public void grow(Map<Direction, Particle> neighbors) {
+    }
+
+    public void burn(Map<Direction, Particle> neighbors) {
+    }
+
+    public void action(Map<Direction, Particle> neighbors) {
+    }
 }
