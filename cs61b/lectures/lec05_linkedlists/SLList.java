b74d853e2ada7e7ca69dff185625d5b5051f22c6
AdityaMane-cell <adityamane.sae.comp@gmail.com>
2026-09-12 11:05:14 +0530

    feat: adding public size and helper size methods

--- cs61b/lectures/lec05_linkedlists/SLList.java
+++ cs61b/lectures/lec05_linkedlists/SLList.java
@@ -19,13 +19,13 @@ public class SLList {
     public static void main(String[] args) {
         SLList l = new SLList(10);
 
-        // creating list {2, 4, 5,7, 10 }
+        // creating list {3, 5,7, 10 }
         l.addFirst(7);
         l.addFirst(5);
-        l.addFirst(4);
-        l.addFirst(2);
+        l.addFirst(3);
 
-        System.out.println(l); // testing addfirst & toString()
+        System.out.println(l); // testing addfirst() & toString()
+        System.out.println("Size: " + l.size()); // testing size()
 
         SLList l2 = new SLList(20);
         l2.addLast(30);
@@ -33,7 +33,8 @@ public class SLList {
         l2.addLast(50);
         l2.addLast(60);
 
-        System.out.println(l2); //testing addlast
+        System.out.println(l2); // testing addlast()
+        System.out.println("Size: " + l2.size());
 
     }
 
@@ -54,6 +55,18 @@ public class SLList {
             temp = temp.next;
         }
         temp.next = new IntNode(x, null);
+    }
+
+    // helper for size
+    public static int size(IntNode p) {
+        if (p.next == null) {
+            return 1;
+        }
+        return 1 + size(p.next);
+    }
+
+    public int size() {
+        return size(first);
     }
 
     @Override
