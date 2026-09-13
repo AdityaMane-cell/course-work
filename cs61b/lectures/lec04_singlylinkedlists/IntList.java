7e924721e544cb760486318264f61f442c8e9b63
AdityaMane-cell <adityamane.sae.comp@gmail.com>
2026-09-13 11:17:22 +0530

    concluding study on singly LinkedLists

---
+++ cs61b/lectures/lec04_singlylinkedlists/IntList.java
@@ -0,0 +1,115 @@
+class IntList {
+    int first;
+    IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    public int iterativeSize() {
+        IntList p = this;
+        int size = 1;
+        while (p.rest != null) {
+            size++;
+            p = p.rest;
+        }
+        return size;
+    }
+
+    public int get(int i) {
+        if (i == 0) {
+            return first;
+        }
+        return this.rest.get(i - 1);
+    }
+
+    /* return identical IntList with all values incremented by x .. do not change */
+    public static IntList incrList(IntList l, int x) {
+        IntList marker = l;
+        IntList nl = new IntList(-1, null);
+        IntList temp = nl;
+        int n = l.size();
+
+        for (int i = 0; i < n; i++) {
+            temp.rest = new IntList(marker.first + x, null);
+            marker = marker.rest;
+            temp = temp.rest;
+        }
+        return nl.rest;
+    }
+
+    /* return identical IntList with all values incrementd by x .. save to memory */
+    public static IntList dincrList(IntList l, int x) {
+        IntList m = l;
+        int n = l.size();
+        for (int i = 0; i < n; i++) {
+            m.first = m.first + x;
+            m = m.rest;
+        }
+        return l;
+    }
+
+    // sum
+    public static int sum(IntList first) {
+        int sum = 0;
+        IntList temp = first;
+        while (temp != null) {
+            sum += temp.first;
+            temp = temp.rest;
+        }
+        return sum;
+    }
+
+    // addFirst
+    public IntList addFirst(int x) {
+        return new IntList(x, this);
+    }
+
+    // addLast
+    public void addLast(int x) {
+
+    }
+
+    public static void print(IntList l) {
+        int n = l.size();
+        IntList m = l;
+        System.out.println("Printing list: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(m.first + " -> ");
+            m = m.rest;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        IntList l = new IntList(15, null);
+        l = new IntList(10, l);
+        l = new IntList(5, l);
+
+        System.out.println("Size: " + l.size());
+        System.out.println("Iteration Size: " + l.iterativeSize());
+        System.out.println("I'th element: " + l.get(1));
+
+        // IntList nl = incrList(l, 3);
+        // print(nl);
+
+        // dincrList(l, 3);
+        print(l);
+
+        System.out.println("Total Sum: " + sum(l));
+
+        l = l.addFirst(7);
+        print(l);
+
+        l.addLast(99);
+        print(l);
+    }
+}
---
+++ cs61b/lectures/lec04_singlylinkedlists/SLList.java
@@ -0,0 +1,132 @@
+public class SLList {
+
+    private static class IntNode {
+        private int item;
+        private IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
+    /*
+     * sentinel reference always points to sentinel node
+     * first item if it exists is always at sentinel.next
+     * sentinel is an invariant(something that will always hold true durning code
+     * execution
+     */
+    private IntNode sentinel;
+    private int size;
+
+    public SLList() {
+        sentinel = new IntNode(63, null);
+        size = 0;
+    }
+
+    public SLList(int x) {
+        sentinel = new IntNode(63, null);
+        sentinel.next = new IntNode(x, null);
+        size++;
+    }
+
+    public static void main(String[] args) {
+        SLList l = new SLList(10);
+
+        // creating list {3, 5,7, 10 }
+        l.addFirst(7);
+        l.addFirst(5);
+        l.addFirst(3);
+
+        System.out.println(l); // testing addfirst() & toString()
+        System.out.println("Size: " + l.size()); // testing size()
+
+        SLList l2 = new SLList(20);
+        l2.addLast(30);
+        l2.addLast(40);
+        l2.addLast(50);
+        l2.addLast(60);
+
+        System.out.println(l2); // testing addlast()
+        System.out.println("Size: " + l2.size());
+
+        /*
+         * NullPointerException Error.
+         * It occurs when SLList is intantialized with SLList() instead of SLList(x)
+         * mechanism failure: When addLast() is invoked on the null list it tries to
+         * access the next feild of null which doesn't exist.
+         * solution:
+         * Approach 1 -> add null check in addLast method and if List is null then
+         * invoke addFirst method internally
+         * Approach 2 -> use sentinel node which is actually vastly superior to the
+         * approach 1.
+         * sentinel node eliminates the need for these special cases.
+         * which make the code clear, bug resistent & easier to upgrade (eg. convert to
+         * doubly linkedlist approach1 would result in lots of errors)
+         */
+        SLList l3 = new SLList();
+        l3.addLast(11);
+        System.out.println("Solving the error: " + l3); // testing
+
+    }
+
+    // add x in front of the list
+    public void addFirst(int x) {
+        sentinel.next = new IntNode(x, sentinel.next);
+        size++;
+    }
+
+    public int getFirst() {
+        return sentinel.next.item;
+    }
+
+    public void addLast(int x) {
+        IntNode temp = sentinel;
+
+        /*
+         * approach 1
+         * if(temp == null) {
+         * addFirst(x);
+         * return;
+         * }
+         */
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = new IntNode(x, null);
+        size++;
+    }
+
+    // faster and computationally cheaper method
+    public int size() {
+        return size;
+    }
+
+    // // helper for size
+    // public static int size(IntNode p) {
+    // if (p.next == null) {
+    // return 1;
+    // }
+    // return 1 + size(p.next);
+    // }
+    // public int size() {
+    // return size(first);
+    // }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        IntNode temp = sentinel.next;
+        while (temp != null) {
+            sb.append(temp.item);
+            if (temp.next != null)
+                sb.append(" -> ");
+            temp = temp.next;
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+}
