7a9a86e2e693c9cd3833bea901bc04058dda17e8
AdityaMane-cell <adityamane.sae.comp@gmail.com>
2026-09-13 11:17:53 +0530

    Starting study of Doubly LinkedLists

---
+++ cs61b/lectures/lec05_doublylinkedlists/DLLists.java
@@ -0,0 +1,12 @@
+public class DLLists {
+    private static IntNode{
+
+    }
+    private InNode first;
+    private IntNode last;
+    private IntNode size;
+    
+    public static void main(String[] args) {
+
+    }
+}
